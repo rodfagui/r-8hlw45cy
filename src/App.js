7f0e9f28be3c4c34039e9445ffaab4e20f9a6ecb
Rodman Aguirre <rodfagui@gmail.com>
2017-08-28 01:03:54 -0500

    Finish

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class Welcolme extends Component {
-  render() {
-    return (
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    );
-  }
-}
-
 class App extends Component {
   constructor() {
     super();
     this.state = { 
-      show: false 
+      showForm: false 
     };
   }
   render() {
+    const showHide = {
+      'display': this.state.showForm ? 'block' : 'none'
+    };
+
     return (
       <div className="wrapper">
-        <label><input type="checkbox" checked={this.state.show} onClick={this.showText.bind(this)} /> Mostrar información importante</label>
-        {this.state.show ? <Welcolme /> : null }
+        <label><input type="checkbox" checked={this.state.showForm} onClick={this.showText.bind(this)} /> Mostrar información importante</label>
+        <p style={showHide}>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
     );
   }
   showText(event) {
-    this.setState({show: event.target.checked});
+    this.setState({showForm: event.target.checked});
   }
 }
 
